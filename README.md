44109954d9a3dc9185771fb49c16754e07710dca
Sir-aji josh <joshman.aji@gmail.com>
2023-11-22 17:40:56 -0800

    all files

---
+++ README.md
@@ -0,0 +1,4 @@
+HBNB - The Console
+This is a group project forked by Jordan Lonpolidormor Mefujie. Together with Joshua Ajiferuke.
+
+Project name: AirBnB_clone_v2
---
+++ models/city.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This is the city class.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
+
+
+class City(BaseModel, Base):
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphans',
+                          backref="cities")
+
+        @property
+        def amenities(self):
+            """ Returns list of amenity ids """
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ Appends amenity ids to the attribute """
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
