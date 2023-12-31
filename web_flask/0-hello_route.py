63b7e122fb9e0d05af9207e38accfba6dc04beb1
Sir-aji-josh <joshman.aji@gmail.com>
2023-12-31 05:09:47 -0800

    task 0 - My first commit.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-""" A script thats starts a Flask web application.
+""" class Flask.
 """
+
 from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-app = Flask(_name_)
 
-
-# Route to display "Hello HBNB!"
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    return 'Hello HBNB!'
+    """ A function that returns the string 'Hello HBNB!'.
+    """
+    return "Hello HBNB!"
 
-if _name_ == '_main_':
-    # Run the web application on 0.0.0.0, port 5000
-    app.run(host='0.0.0.0', port=5000)
 
+@app.route('/hbnb')
+def hbnb():
+    """ A function that returns the string 'HBNB'.
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
