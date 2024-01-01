bc5fd7baecf1f960ff89048be794809ff1af6a88
Sir-aji-josh <joshman.aji@gmail.com>
2024-01-01 14:00:18 -0800

    task 6. Odd or even.

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" A script thats starts a Flask web application.
+""" A script thats starts a Flask web application.`
+The application listens on 0.0.0.0, port 5000.
 """
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
